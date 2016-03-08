f707a4f880d3d6dc58cfd8b35cfc73b6ac265a1d
Fettn <fettn@baerinteractive.de>
2016-03-09 00:23:04 +0100

    changing to push the artifact to central

--- lib/src/test/java/de/baerinteractive/pixlhash/PixlHashTest.java
+++ lib/src/test/java/de/baerinteractive/pixlhash/PixlHashTest.java
@@ -12,18 +12,18 @@ public class PixlHashTest {
         assertEquals(1, result[2]);
 
         result = PixlHash.getPixelData(new byte[]{(byte)0xAB, 0x10}, 1);
-        assertEquals(3, result[0]);
-        assertEquals(5, result[1]);
-        assertEquals(0, result[2]);
-
-        result = PixlHash.getPixelData(new byte[]{(byte)0xAB, 0x10}, 2);
         assertEquals(6, result[0]);
         assertEquals(10, result[1]);
         assertEquals(0, result[2]);
 
+        result = PixlHash.getPixelData(new byte[]{(byte)0xAB, 0x10}, 2);
+        assertEquals(0, result[0]);
+        assertEquals(11, result[1]);
+        assertEquals(1, result[2]);
+
         result = PixlHash.getPixelData(new byte[]{(byte)0xAB, 0x10}, 3);
-        assertEquals(4, result[0]);
-        assertEquals(5, result[1]);
+        assertEquals(2, result[0]);
+        assertEquals(12, result[1]);
         assertEquals(0, result[2]);
     }
 }
